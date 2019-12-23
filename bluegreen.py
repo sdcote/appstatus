2abd90ecd80241beacb6d4134a8e7a5bb8f62cf6
sdcote <sdcote@aep.com>
2019-12-23 16:27:15 -0500

    minor changes to parameters

--- bluegreen.py
+++ bluegreen.py
@@ -2,20 +2,20 @@ import argparse
 from AppStatus import Plan
 
 parser = argparse.ArgumentParser(description='Modify the AppStatus of deploument nodes')
-parser.add_argument("method", nargs='*', default="status",
-                    choices=["forward", "back", "commit", "single", "rolling", "status"],
-                    help="the type of deployment (default = status)")
+parser.add_argument("action", nargs='*', default="status", choices=["forward", "rollback", "commit", "single", "rolling", "goal", "status"], help="the type of action to take (default = status)")
 parser.add_argument("-v", "--verbose", action="store_true", default=False, help="verbose output")
 parser.add_argument("-f", "--file", help="file name for the plan (default = plan.json)")
 parser.add_argument("-u", "--username", help="Username credential")
 parser.add_argument("-p", "--password", help="Password credential")
+parser.add_argument("-n", "--node", help="Execute on one node only")
+parser.add_argument("-s", "--status", help="The status to set")
 
 args = parser.parse_args()
 if args.verbose:
     print "verbosity turned on"
 
 filename = "plan.json"
-if args.file != None:
+if args.file is not None:
     filename = args.file
 
 if args.verbose:
@@ -26,4 +26,5 @@ plan = Plan(filename)
 if args.verbose:
     print "The plan contains " + str(len(plan.nodes)) + " nodes"
 
-plan.show_current()
+if args.action == "status":
+    plan.show_current()
